61aa1faaedfaeea8f991de15587ba741c675ae17
HeldyisAE <heaguero@estudiantec.cr>
2026-05-01 00:52:20 -0600

    Implementacion del menu de busqueda con validaciones

--- Programa/src/Helpers/OperacionesFechas.hs
+++ Programa/src/Helpers/OperacionesFechas.hs
@@ -3,6 +3,14 @@ module Helpers.OperacionesFechas where
 
 import Data.Time
 import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds, posixSecondsToUTCTime)
+import Data.Char (isDigit)
+
+formatoValido :: String -> Bool
+formatoValido str =
+    case str of 
+        [d1,d2,'-',m1,m2,'-',y1,y2,y3,y4] ->
+            all isDigit [d1,d2,m1,m2,y1,y2,y3,y4]
+        _ -> False
 
 formatearFecha :: Int -> String
 formatearFecha timestamp =
@@ -18,7 +26,10 @@ timestampConvertir fecha =
     round (utcTimeToPOSIXSeconds fecha) -- Convierte de fecha a segundos
 
 convertirFecha :: String -> Maybe Int 
-convertirFecha str = do 
-    fecha <- parsearFecha str -- Convierte el string a fecha
-    return (timestampConvertir fecha) -- Convierte el string el timestamp para buscar
+convertirFecha str =
+    if not (formatoValido str)
+        then Nothing
+        else do
+            fecha <- parsearFecha str -- Convierte el string a fecha
+            return (timestampConvertir fecha) -- Convierte el string el timestamp para buscar
 
