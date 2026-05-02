22be1eb42ccfce806efceecc7f41c6f1631be068
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2026-05-02 15:07:36 -0600

    transformacion arreglada

--- Programa/src/Logic/Transformacion.hs
+++ Programa/src/Logic/Transformacion.hs
@@ -3,7 +3,10 @@ module Logic.Transformacion where
 import Types.Event
 import Logic.AnalisisDatos (promedioCategoriaAnual)
 
--- 1. Aplicar impuesto del 13% solo a compras
+--Aplicar impuesto(13%)
+-- En esta se maneja la lista de eventos y cuando se calcula todo lo que devuelve es la lista pero con los cambios aplicados
+-- tecnicamente lo que hace es a partir de la lista original va creando una con los cambios 
+-- y esa es la que manda de vuelta al menu
 aplicarImpuesto :: [Event] -> [Event]
 aplicarImpuesto eventos = map transformar eventos
   where
@@ -11,8 +14,7 @@ aplicarImpuesto eventos = map transformar eventos
         | category e == "compra" = e { value = value e * 1.13 }
         | otherwise              = e
 
--- 2. Etiquetar eventos de alto valor[cite: 1]
--- Filtra eventos que superan el promedio de su categoría
+--Etiquetar eventos de alto valor
 eventosAltoValor :: [Event] -> [Event]
 eventosAltoValor eventos = 
     filter (\e -> value e > obtenerPromedio (category e)) eventos
--- Programa/src/UI/MenuTransformacion.hs
+++ Programa/src/UI/MenuTransformacion.hs
@@ -5,7 +5,6 @@ import Types.Event
 import Logic.Transformacion
 import Logic.Busqueda (mostrarEvento)
 
--- Esta función maneja el menú y devuelve la lista (actualizada o no) al menú principal
 menuTransformacion :: [Event] -> IO [Event]
 menuTransformacion eventos = do
     putStrLn "\n--- Transformación de eventos ---"
@@ -18,28 +17,24 @@ menuTransformacion eventos = do
     opcion <- getLine
     procesarTransOp opcion eventos
 
--- Aquí se procesa la elección del usuario
 procesarTransOp :: String -> [Event] -> IO [Event]
 procesarTransOp opcion eventos =
     case opcion of
         "1" -> do
             let eventosTransformados = aplicarImpuesto eventos
-            putStrLn "\n[!] Impuesto del 13% aplicado a las compras con éxito."
-            -- RECURSIÓN: Te quedas en el submenú con los datos nuevos
+            putStrLn "\n[!] Impuesto(13%) aplicado a las compras."
             menuTransformacion eventosTransformados
 
         "2" -> do
-            -- Filtramos los eventos que superan el promedio de su categoría[cite: 11]
+            -- Filtra los eventos que superan el promedio
             let altosValores = eventosAltoValor eventos
-            putStrLn "\n--- Eventos de Alto Valor (Sobre el promedio) ---"
+            putStrLn "\n--- Eventos de Alto Valor---"
             if null altosValores
                 then putStrLn "No hay eventos que superen el promedio actual."
                 else mapM_ (putStr . mostrarEvento) altosValores
-            -- Aquí no modificamos la lista, solo la mostramos, así que pasamos la original[cite: 12]
             menuTransformacion eventos
 
         "0" -> do
-            -- Retornamos la lista final (con o sin impuestos) para que el Principal la use[cite: 11, 12]
             return eventos
 
         _ -> do
