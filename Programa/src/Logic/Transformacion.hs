c6aa22f506d2d9b1a203b03f87ef6161d97135f7
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2026-05-02 16:53:09 -0600

    Update Transformacion.hs

--- Programa/src/Logic/Transformacion.hs
+++ Programa/src/Logic/Transformacion.hs
@@ -11,7 +11,6 @@ aplicarImpuesto :: [Event] -> [Event]
 aplicarImpuesto eventos = map transformar eventos
   where
     transformar e
-        -- Cambia 'categoria' por 'category' y 'valor' por 'value'
         | category e == "compra" && tax e == 0 = 
             let montoImpuesto = value e * (0.13 :: Float)
             in e { value = value e + montoImpuesto, tax = montoImpuesto }
