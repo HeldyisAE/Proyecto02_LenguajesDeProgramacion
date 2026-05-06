8549c068d7422f8e9bca9672a80fc42063f90aa3
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2026-05-05 19:53:54 -0600

    Reapply "Merge branch 'Transformación-de-eventos-funcion' into develop"
    This reverts commit 57f95f9abb7d0a3579787fd242adc652410eaaf2.

--- Programa/src/Logic/Transformacion.hs
+++ Programa/src/Logic/Transformacion.hs
@@ -18,12 +18,19 @@ aplicarImpuesto eventos = map transformar eventos
         | otherwise = e
 
 --Etiquetar eventos de alto valor
+-- actualiza las etiquetas de los de alto valor para saber cuales si y cuales no con elo booleano
 eventosAltoValor :: [Event] -> [Event]
-eventosAltoValor eventos = 
-    filter (\e -> value e > obtenerPromedio (category e)) eventos
+eventosAltoValor eventos = map etiquetar eventos
   where
-    promedios = promedioCategoriaAnual eventos 
+    promedios = promedioCategoriaAnual eventos
+    
     obtenerPromedio cat = 
         case filter (\(c, _, _) -> c == cat) promedios of
             ((_, _, p):_) -> p
             []            -> 0
+
+    etiquetar e =
+        let promedio = obtenerPromedio (category e)
+        in if value e > promedio
+           then e { tag = True }
+           else e
